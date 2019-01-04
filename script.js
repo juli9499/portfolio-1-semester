994aec480c553723cb35afb780d00cdf949444e1
Julie Hansen <juliehansen@desktop-drcflrr.kea.local>
2019-01-04 11:03:56 +0100

    init

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", sidenVises);
+
+
+
+function sidenVises() {
+    console.log("siden vises");
+    //registerer klik på menu-knap
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("Toggle menu");
+    document.querySelector("#undermenu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#undermenu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        //menuen er nu skjult - ændre menuknap til lll
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        //menuen er nu vist - ændre menuknap til X
+        document.querySelector("#menuknap").textContent = "X";
+    }
+
+}
